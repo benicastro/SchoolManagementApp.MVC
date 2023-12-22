e02004010816a966b07aa79642dabd1e309f3f03
benicastro <benedict.zcastro@gmail.com>
2023-12-23 02:03:51 +0800

    Established connection with app

--- Data/SchoolManagementContext.cs
+++ Data/SchoolManagementContext.cs
@@ -6,10 +6,6 @@ namespace SchoolManagementApp.MVC.Data;
 
 public partial class SchoolManagementContext : DbContext
 {
-    public SchoolManagementContext()
-    {
-    }
-
     public SchoolManagementContext(DbContextOptions<SchoolManagementContext> options)
         : base(options)
     {
@@ -20,10 +16,6 @@ public partial class SchoolManagementContext : DbContext
     public virtual DbSet<Lecturer> Lecturers { get; set; }
 
     public virtual DbSet<Student> Students { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=milytoni\\sqlexpress; Database=SchoolManagement;Trusted_Connection=true;MultipleActiveResultSets=true;Encrypt=false");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {

7423f525463e51456cf6e7228e279c7fb8181e48
benicastro <benedict.zcastro@gmail.com>
2024-01-02 17:02:27 +0800

    Integrated Auth0 in the app

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
+using System.Configuration;
+using Auth0.AspNetCore.Authentication;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using SchoolManagementApp.MVC.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +9,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 var conn = builder.Configuration.GetConnectionString("SchoolManagementDbConnection");
 builder.Services.AddDbContext<SchoolManagementContext>(q => q.UseSqlServer(conn));
+
+
+builder.Services.AddAuth0WebAppAuthentication(options =>
+{
+    options.Domain = builder.Configuration["Auth0:Domain"];
+    options.ClientId = builder.Configuration["Auth0:ClientId"];
+});
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -23,6 +34,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
